e75e29b88288f3468aabb5e4e53290a96ecb9ba4
aosman <pianogreen0@gmail.com>
2022-10-18 17:56:02 +0100

    tweaking profile page

--- client/src/pages/Profiles.js
+++ client/src/pages/Profiles.js
@@ -1,53 +1,45 @@
-import React, { useEffect } from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { getProfile } from '../features/profiles/profileSlice'
-import {Container,Col,Row,Form} from "react-bootstrap"
-import {toast} from "react-toastify"
-import SpinnerComponent from '../components/SpinnerComponent'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProfile } from "../features/profiles/profileSlice";
+import { Container, Col, Row, Form } from "react-bootstrap";
+import { toast } from "react-toastify";
+import SpinnerComponent from "../components/SpinnerComponent";
 export default function Profiles() {
-  const {profile,isLoading,isSuccess,isError,message} = useSelector((state)=> state.profile)
-  
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    
+  const { profile, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.profile
+  );
+
+  const dispatch = useDispatch();
+  useEffect(() => {
     //  toast.error(message)
     //  console.log(message);
-    if(isError){
-      toast.error("You need to be logged in ")
-    }
-    else{
-      dispatch(getProfile())
+    if (isError) {
+      toast.error("You need to be logged in ");
+    } else {
+      dispatch(getProfile());
       // console.log(profile);
-
     }
-
-  
-  ;  
-  },[dispatch,isError,message])
+  }, [dispatch, isError, message]);
   console.log(profile);
-  
-  return <div>
-    
-    <Container>
-      {isLoading && <SpinnerComponent/>}
-     
 
-   <Row className='mt-5'>
-      <h1 className='mt-5'>General Information</h1>
-    
-    <Col>
-    
+  return (
+    <div>
+      <Container>
+        {isLoading && <SpinnerComponent />}
 
-    
-    <Form.Group className="mb-3">
-        <Form.Label className='mt-3'>First Name</Form.Label>
-        <Form.Control placeholder={`${profile.first_name}`} disabled />
-        <Form.Label className='mt-3'>Last Name</Form.Label>
-        <Form.Control placeholder={`${profile.last_name}`} disabled />
-        <Form.Label className='mt-3'>Email</Form.Label>
-        <Form.Control placeholder={`${profile.email}`} disabled />
-      </Form.Group>
-      {/* <Form.Group className="mb-3">
+        <Row className="mt-5">
+          <h1 className="mt-5">General Information</h1>
+
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label className="mt-3">First Name</Form.Label>
+              <Form.Control placeholder={`${profile.first_name}`} disabled />
+              <Form.Label className="mt-3">Last Name</Form.Label>
+              <Form.Control placeholder={`${profile.last_name}`} disabled />
+              <Form.Label className="mt-3">Email</Form.Label>
+              <Form.Control placeholder={`${profile.email}`} disabled />
+            </Form.Group>
+            {/* <Form.Group className="mb-3">
         <Form.Label>Disabled select menu</Form.Label>
         <Form.Select disabled>
         <option>Disabled select</option>
@@ -56,46 +48,50 @@ export default function Profiles() {
         <Form.Group className="mb-3">
         <Form.Check type="checkbox" label="Can't check this" disabled />
       </Form.Group> */}
-    </Col>
-    <Col >
-    <Form.Group className="mb-3">
-        <Form.Label className='mt-3'>Username</Form.Label>
-        <Form.Control placeholder={`${profile.username}`} disabled />
-        <Form.Label className='mt-3'>Gender</Form.Label>
-        <Form.Control placeholder={`${profile.gender}`} disabled />
-        <Form.Label className='mt-3'>Phone Number</Form.Label>
-        <Form.Control placeholder={`${profile.phone_number}`} disabled />
-      </Form.Group>
-    </Col>
-   </Row>
-     <Container>
-      <h1>Address</h1>
-      <Row>
-        <Col>
-     <Form.Group className="mb-3">
-     <Form.Label className='mt-3'>Country</Form.Label>
-     <Form.Control placeholder={`${profile.country}`} disabled />
-
-     </Form.Group>
-        
-        </Col>
-      </Row>
-     </Container>
-     <Row>
-        <Col sm={8}>
-        <Form.Group className="mb-3">
-          <Form.Label>City</Form.Label>
-          <Form.Control placeholder={`${profile.city}`} disabled />
-          </Form.Group>
-        </Col>
-        <Col sm={4}>
-        <Form.Group className="mb-3">
-          <Form.Label>City</Form.Label>
-          <Form.Control placeholder={`${profile.city}`} disabled />
-          </Form.Group>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-  
+          </Col>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label className="mt-3">Username</Form.Label>
+              <Form.Control placeholder={`${profile.username}`} disabled />
+              <Form.Label className="mt-3">Gender</Form.Label>
+              <Form.Control placeholder={`${profile.gender}`} disabled />
+              <Form.Label className="mt-3">Phone Number</Form.Label>
+              <Form.Control placeholder={`${profile.phone_number}`} disabled />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Container>
+          <h1>Address</h1>
+          <Row>
+            <Col sm={8}>
+              <Form.Group className="mb-3">
+                <Form.Label>Country</Form.Label>
+                <Form.Control placeholder={`${profile.country}`} disabled />
+              </Form.Group>
+            </Col>
+            <Col sm={4}>
+              <Form.Group className="mb-3">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder={`${profile.city}`} disabled />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Container>
+        <Row>
+          <Col><Form.Group className="mb-3">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder={`${profile.city}`} disabled />
+              </Form.Group></Col>
+          <Col><Form.Group className="mb-3">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder={`${profile.city}`} disabled />
+              </Form.Group></Col>
+          <Col><Form.Group className="mb-3">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder={`${profile.city}`} disabled />
+              </Form.Group></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
