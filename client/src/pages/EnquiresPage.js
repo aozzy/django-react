a4322a7c2828dcc5516fbac98fc96886350b3dd7
aosman <pianogreen0@gmail.com>
2022-11-03 15:54:01 +0000

    changes to enquires page

--- client/src/pages/EnquiresPage.js
+++ client/src/pages/EnquiresPage.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Title from '../components/Title'
+import {Col,Container,Row} from "react-bootstrap"
 
 export default function EnquiresPage() {
   return (
     <React.Fragment>
       <Title title="Enquires Page">
+<Container>
+  <Row>
+    <Col>
      <h1>Enquires Page</h1>
-
+    
+    </Col>
+  </Row>
+</Container>
       </Title>
     </React.Fragment>
   )
